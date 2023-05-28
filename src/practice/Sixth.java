207dc4b2b74aa89388c04594cd8485a3f031ad1e
dbflwkdj7@naver.com <dbflwkdj7@naver.com>
2023-05-29 02:09:05 +0900

    0528 Late

---
+++ src/practice/Sixth.java
@@ -0,0 +1,57 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Sixth {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+		int a[] = new int[6];
+		int b[] = new int[6];
+		String c[] = new String[6];
+		String answer = "";
+		//StirngTokenizer가 안에 있으면 세로로 입력해야
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < 6; i++) {
+			int input = Integer.parseInt(st.nextToken());
+			a[i] = input;
+			
+			if(i == 0 || i == 1) {
+				b[i] = 1;
+			}else if(i == 2 || i == 3 || i == 4) {
+				b[i] = 2;
+			}else if(i == 5) {
+				b[i] = 8;
+			}
+			
+			c[i] = Integer.toString(b[i] - a[i]);
+		}
+		
+		for(int i = 0; i < c.length - 1; i++) {
+			answer = answer + c[i] + " ";
+		}
+		answer = answer + c[5];
+		
+		
+		System.out.println(answer);
+		
+		System.out.println(Arrays.toString(c));
+		// 사이사이마다 넣어주는 듯
+		System.out.println(String.join(" ", c)); //개 쩐다!~!!
+
+		
+		
+//		System.out.println("         ,r'\"7");
+//		System.out.println("r`-_   ,'  ,/");
+//		System.out.println(" \\. \". L_r'");
+//		System.out.println("   `~\\/");
+//		System.out.println("      |");
+//		System.out.println("      |");
+	}
+	
+}
