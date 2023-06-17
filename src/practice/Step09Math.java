531a2b20243c15da24ee0c5c10b734dcdff2f83b
dbflwkdj7@naver.com <dbflwkdj7@naver.com>
2023-06-17 21:50:33 +0900

    0617 Math

--- src/practice/Step09Math.java
+++ src/practice/Step09Math.java
@@ -1,26 +1,55 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Step09Math {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+		int A = 0;
 		while(true) {
-			int A = scan.nextInt();
-			int B = scan.nextInt();
-			
-			if(A == 0 && B == 0) {
+			A = scan.nextInt();
+			if(A == -1) {
 				break;
-			}else if(B % A == 0) {
-				System.out.println("factor");
-			}else if(A % B == 0) {
-				System.out.println("multiple");				
-			}else {
-				System.out.println("neither");
 			}
+			System.out.println(1);
 		}
+		
+		
+		
+		
+		
+		
+//		int A = scan.nextInt();
+//		int B = scan.nextInt();
+////		ArrayList<Integer> arr = new ArrayList<Integer>(); 
+//		int [] arr = new int [A];
+//		int index = 0;
+//		for(int i = 0; i < A; i++) {
+//			if(A % (i+1) == 0) {
+//				arr[index] = i+1;
+//				index++;
+//			}
+//		}
+//		System.out.println(Arrays.toString(arr));
+//		System.out.println(arr[B - 1]);
+		
+//		while(true) {
+//			int A = scan.nextInt();
+//			int B = scan.nextInt();
+//			
+//			if(A == 0 && B == 0) {
+//				break;
+//			}else if(B % A == 0) {
+//				System.out.println("factor");
+//			}else if(A % B == 0) {
+//				System.out.println("multiple");				
+//			}else {
+//				System.out.println("neither");
+//			}
+//		}
 		
 
 	}
