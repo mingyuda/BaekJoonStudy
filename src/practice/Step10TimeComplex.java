8ea7cb9ef8a5209c772ec9d526cb5963b83716da
dbflwkdj7@naver.com <dbflwkdj7@naver.com>
2023-06-28 15:30:38 +0900

    0628

---
+++ src/practice/Step10TimeComplex.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Step10TimeComplex {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		System.out.println(1);
+		System.out.println(0);
+
+	}
+
+}
