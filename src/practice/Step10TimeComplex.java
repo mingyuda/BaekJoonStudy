c362276d3339e6e612120aec435f65dc16ec6d50
dbflwkdj7@naver.com <dbflwkdj7@naver.com>
2023-07-07 23:21:03 +0900

    0707

--- src/practice/Step10TimeComplex.java
+++ src/practice/Step10TimeComplex.java
@@ -14,35 +14,131 @@ public class Step10TimeComplex {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int input = scan.nextInt();
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int d = scan.nextInt();
+		int e = scan.nextInt();
+		int f = scan.nextInt();
 		
-		int copy = input;
-		int temp = copy;
-		int answer = 0;
-		while(temp > 0) {
-			int len = Integer.toString(temp).length();
-			int [] arr = new int[len];
-			int index = (len-1);
-			int temp2 = temp;
-			while(temp2 > 0) {
-				arr[index] = temp2 % 10;
-				temp2 /=10;
-				index--;
-			}
-			int sum = 0;
-			for(int i = 0; i < arr.length; i++) {
-				sum += arr[i];
-			}
-			sum += temp;
-			if(sum == copy) {
-				answer = temp;
-			}
-			
-			temp--;
-		}
+		// 크래머의 공식
+		int D1 = a * e - (d * b);
+		int D2 = c * e - (f * b);
+		int D3 = a * f - (d * c);
+		
+		int x = D2/D1;
+		int y = D3/D1;
+		
+		System.out.print(x + " ");
+		System.out.print(y);
 		
 		
-		System.out.println(answer);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		실패
+//		int a = scan.nextInt();
+//		int b = scan.nextInt();
+//		int c = scan.nextInt();
+//		int d = scan.nextInt();
+//		int e = scan.nextInt();
+//		int f = scan.nextInt();
+//		
+//		int answer_x = 0;
+//		int answer_y= 0;
+//		if(a != 0) {
+//			for(int i = 0; i < 1000; i++) {
+//				if(((c-b*i)/a) * d + (e*i) == f) {
+//					answer_y = i;
+//					answer_x = (c-b*i)/a;
+//	
+//					break;
+//				}
+//			}
+//			for(int i = -999; i < 0; i++) {
+//				if(((c-b*i)/a) * d + (e*i) == f) {
+//					answer_y = i;
+//					answer_x = (c-b*i)/a;
+//					break;
+//				}
+//			}
+//		}else if(d != 0){
+//			for(int i = 0; i < 1000; i++) {
+//				if(b * i == c) {
+//					answer_y = i;
+//					answer_x = (e * i - f) / d;
+//	
+//					break;
+//				}
+//			}
+//			for(int i = -999; i < 0; i++) {
+//				if(b * i == c) {
+//					answer_y = i;
+//					answer_x = (e * i - f) / d;
+//					break;
+//				}
+//			}	
+//		}else {
+//			answer_x = 0;
+//			for(int i = 0; i < 1000; i++) {
+//				if(b * i - (e * i) == c - f) {
+//					answer_y = i;
+//	
+//					break;
+//				}
+//			}
+//			for(int i = -999; i < 0; i++) {
+//				if(b * i - (e * i) == c - f) {
+//					answer_y = i;
+//	
+//					break;
+//				}
+//			}
+//		}
+//		
+//		
+//		
+//		
+//		System.out.print(answer_x + " ");
+//		System.out.print(answer_y);
+		
+//		int input = scan.nextInt();
+//		
+//		int copy = input;
+//		int temp = copy;
+//		int answer = 0;
+//		while(temp > 0) {
+//			int len = Integer.toString(temp).length();
+//			int [] arr = new int[len];
+//			int index = (len-1);
+//			int temp2 = temp;
+//			while(temp2 > 0) {
+//				arr[index] = temp2 % 10;
+//				temp2 /=10;
+//				index--;
+//			}
+//			int sum = 0;
+//			for(int i = 0; i < arr.length; i++) {
+//				sum += arr[i];
+//			}
+//			sum += temp;
+//			if(sum == copy) {
+//				answer = temp;
+//			}
+//			
+//			temp--;
+//		}
+//		
+//		
+//		System.out.println(answer);
 		
 		
 		
