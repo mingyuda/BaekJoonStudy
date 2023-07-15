fbab7ecc109502ca4b66adcbc7b5c1279b0f3df6
dbflwkdj7@naver.com <dbflwkdj7@naver.com>
2023-07-15 23:37:56 +0900

    0715

--- src/practice/Step11.java
+++ src/practice/Step11.java
@@ -5,26 +5,31 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Step11 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int len = Integer.parseInt(br.readLine());
 		
-		int[] arr = new int[len];
+		ArrayList<Integer> arr = new ArrayList<Integer>();
 		
-		for(int i = 0; i < len; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+		String input = (br.readLine());
+		
+		for(int i = 0; i < input.length(); i++) {
+			arr.add(Integer.parseInt(input.substring(i, i + 1)));
 		}
 		
-		Arrays.sort(arr);
+		Collections.sort(arr);
+		
+		Collections.reverse(arr);
 		
 		StringBuilder stringBuilder = new StringBuilder();
 		for(int value : arr) {
-			stringBuilder.append(value).append('\n');
+			stringBuilder.append(value);
         }
 		
 		System.out.println(stringBuilder);
