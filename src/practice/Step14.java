4c684eea6f8ca77a895616d944d59c77f746bb9c
dbflwkdj7@naver.com <dbflwkdj7@naver.com>
2023-07-23 00:02:17 +0900

    0723

--- src/practice/Step14.java
+++ src/practice/Step14.java
@@ -3,14 +3,24 @@ package practice;
 import java.util.Scanner;
 
 public class Step14 {
+	
+	static int result = 1;
+	static int square(int a) {
+		
+		for(int i = 0; i < a; i++) {
+			result = 2 * result;
+		}
+		
+		
+		return result;
+	}
+	
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		//nC2
 		int input = scan.nextInt();
 		
-		int result = (input * (input-1));
-		
-		System.out.println(result);
+		System.out.println(square(input));
 		
 		
 	}
