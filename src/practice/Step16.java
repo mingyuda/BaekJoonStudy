2a3eae7fb467eda0aa9cd390e5a1ed085410e6fe
dbflwkdj7@naver.com <dbflwkdj7@naver.com>
2023-07-31 23:55:16 +0900

    0731

--- src/practice/Step16.java
+++ src/practice/Step16.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.StringTokenizer;
@@ -11,45 +12,140 @@ import java.util.StringTokenizer;
 public class Step16 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-//		Scanner scan = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		HashMap<Integer, Integer> map = new HashMap<>();
-		HashMap<Integer, Integer> map2 = new HashMap<>();
-		ArrayList<Integer> arr = new ArrayList<Integer>();
+		HashMap<String, String> map = new HashMap<>();
+		ArrayList<String> arr = new ArrayList<String>();
 		int N = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		for(int i = 0; i < N; i++) {
-			int input = Integer.parseInt(st.nextToken());
-			map.put(input, 1);
-		}
-		
-		int M = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st2 = new StringTokenizer(br.readLine());
-		for(int i = 0 ; i < M; i++) {
-			int input = Integer.parseInt(st2.nextToken());
-			arr.add(input);
-			if(map.containsKey(input)) {
-				map2.put(input, 1);
-			}else {
-				map2.put(input, 0);
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String name = st.nextToken(); 
+			String status = st.nextToken();
+			if(status.equals("enter")) {
 			}
+			map.put(name, status);
 			
 		}
 		
+
+		
+		for(String answer : map.keySet()) {
+			if(map.get(answer).equals("enter")) {
+				arr.add(answer);				
+			}
+		}
+		
+		Collections.sort(arr);
+		Collections.reverse(arr);
+		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < map2.size(); i++) {
-			int answer = map2.get(arr.get(i));
-			sb.append(answer + " ");
+		for(int i = 0; i < arr.size(); i++) {
+			sb.append(arr.get(i) + "\n");
 		}
 		
 		System.out.println(sb);
 		
-		//순서가 이상하게 나옴
-//		for(int answer : map2.values()) {
-//			System.out.print(answer);
+	
+		
+		
+//	 7785 실패
+		// 갯수가 안맞음
+		//		Scanner scan = new Scanner(System.in);
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		HashMap<String, String> map = new HashMap<>();
+//		ArrayList<String> temp = new ArrayList<String>();
+//		int N = Integer.parseInt(br.readLine());
+//		
+//		
+//		for(int i = 0; i < N; i++) {
+//			StringTokenizer st = new StringTokenizer(br.readLine());
+//			String name = st.nextToken(); 
+//			String status = st.nextToken();
+//			if(status.equals("enter")) {
+//				temp.add(name);
+//			}
+//			map.put(name, status);
+//			
 //		}
+//		
+//		Collections.sort(temp);
+//		Collections.reverse(temp);
+//
+//		
+//		StringBuilder sb = new StringBuilder();
+//		for(int i = 0; i < map.size(); i++) {
+//			if(map.get(temp.get(i)).equals("enter")) {
+//				sb.append(temp.get(i) + "\n");
+//			}
+//		}
+//		
+//		System.out.println(sb);
+//		
+		
+//		int count = 0;
+//		
+//		
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		int N = Integer.parseInt(st.nextToken());
+//		int M = Integer.parseInt(st.nextToken());
+//		
+//		for(int i = 0; i < N; i++) {
+//			String input = br.readLine();
+//			map.put(input, 1);
+//		}
+//		
+//		
+//		for(int i = 0 ; i < M; i++) {
+//			String input = br.readLine();
+//			if(map.containsKey(input)) {
+//				count++;
+//			}else {
+//			}
+//			
+//		}
+//		
+//		System.out.println(count);
+		
+		
+		
+		
+		
+		
+//		int N = Integer.parseInt(br.readLine());
+//		
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		for(int i = 0; i < N; i++) {
+//			int input = Integer.parseInt(st.nextToken());
+//			map.put(input, 1);
+//		}
+//		
+//		int M = Integer.parseInt(br.readLine());
+//		
+//		StringTokenizer st2 = new StringTokenizer(br.readLine());
+//		for(int i = 0 ; i < M; i++) {
+//			int input = Integer.parseInt(st2.nextToken());
+//			arr.add(input);
+//			if(map.containsKey(input)) {
+//				map2.put(input, 1);
+//			}else {
+//				map2.put(input, 0);
+//			}
+//			
+//		}
+//		
+//		StringBuilder sb = new StringBuilder();
+//		for(int i = 0; i < map2.size(); i++) {
+//			int answer = map2.get(arr.get(i));
+//			sb.append(answer + " ");
+//		}
+//		
+//		System.out.println(sb);
+//		
+//		//순서가 이상하게 나옴
+////		for(int answer : map2.values()) {
+////			System.out.print(answer);
+////		}
 		
 		
 	}
