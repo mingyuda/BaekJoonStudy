59ef4941b074cf201af7911c8b6f9fcfd6c55c08
dbflwkdj7@naver.com <dbflwkdj7@naver.com>
2023-08-17 20:36:36 +0900

    0817

--- src/basicpractice/Step17.java
+++ src/basicpractice/Step17.java
@@ -6,20 +6,54 @@ import java.io.InputStreamReader;
 
 public class Step17 {
 	
-	public static int fibonacci(int x) {
-		
-		if(x == 1 || x == 2) {
+	public static int recursion(String s, int i, int r) {
+		if(i >= r) {
 			return 1;
 		}
 		
-		if(x == 0) {
+		else if(s.charAt(i) != s.charAt(r)) {
 			return 0;
 		}
 		
-		int sum = fibonacci(x-1) + fibonacci(x-2);
-		
-		return sum; 
+		return recursion(s, i + 1, r - 1);
 	}
+	public static int timeCount(String s, int i, int r, int T) {
+		T++;
+		if(i >= r) {
+			return T;
+		}
+		
+		else if(s.charAt(i) != s.charAt(r)) {
+			return T;
+		}
+		return timeCount(s, i + 1, r - 1, T);
+	}
+	
+	public static int isPalindrome(String s) {
+		return recursion(s, 0, s.length() - 1);
+	}
+	
+	
+	public static int time(String s) {
+		return timeCount(s, 0, s.length() - 1, 0);
+	}
+	
+	
+	
+//	public static int fibonacci(int x) {
+//		
+//		if(x == 1 || x == 2) {
+//			return 1;
+//		}
+//		
+//		if(x == 0) {
+//			return 0;
+//		}
+//		
+//		int sum = fibonacci(x-1) + fibonacci(x-2);
+//		
+//		return sum; 
+//	}
 	
 	
 	
@@ -46,7 +80,22 @@ public class Step17 {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		System.out.println(fibonacci(N));
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			String input = br.readLine();
+			sb.append(isPalindrome(input) + " " + time(input)+ "\n");
+		
+		}
+		
+		System.out.println(sb);
+		
+		
+		
+		
+		// Fibonacci
+//		int N = Integer.parseInt(br.readLine());
+//		
+//		System.out.println(fibonacci(N));
 		
 		
 		
